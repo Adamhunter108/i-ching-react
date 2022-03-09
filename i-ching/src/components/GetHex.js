6bd0b4f528c2b14ffb80ec84212d39ecbc0f40c5
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2022-03-09 13:03:06 -0500

    Cleaned up GetHex.js

--- i-ching/src/components/GetHex.js
+++ i-ching/src/components/GetHex.js
@@ -3,22 +3,6 @@ import { Button, Row, Col } from 'react-bootstrap'
 import Hex from '../HexInterpretations'
 
 
-// lets get a random number from 1 - 64
-
-// Between any two numbers
-// Math.floor(Math.random() * (max - min + 1)) + min;
-// const randoVal = Math.floor(Math.random() * (1 - 64 + 1)) + 1;
-
-// Between 0 and max (including 0)
-// Math.floor(Math.random() * (max + 1));
-// const randoVal = Math.floor(Math.random() * (4 + 1))
-
-// Between 1 and max
-// Math.floor(Math.random() * max) + 1;
-// this is the way
-// const randomValue = Math.floor(Math.random() * 64) + 1;
-
-
 
 function GetHex() {
 
@@ -40,17 +24,16 @@ function GetHex() {
         <h1><i class="fa-solid fa-yin-yang"></i></h1>
         <p>kill the noise</p>
 
-
-
-
         <Button variant="dark" size="lg" onClick={getHex}>Divinate</Button>
         <br /><br /><br />
+
         {/* <h1>{value}</h1> */}
 
         {/* this outputs everything from Hex */}
         {/* {Hex.map((data) => (
             <p>{data.id}: {data.title}, {data.description}</p>
         ))} */}
+
 
         {/* need to match value with data.id */}
 
@@ -65,7 +48,6 @@ function GetHex() {
             value === data.id ? <p>Hexagram # {value}</p> : null
                 ))}
 
-
         {/* gets the title */}
         {Hex.map((data) => (
             <h2>{
@@ -73,9 +55,9 @@ function GetHex() {
                 }</h2>
         ))}
 
+        {/* gets the description and contained with Bootstrap */}
         <Row className="justify-content-md-center">
           <Col xs lg="4">
-            {/* gets the description */}
             {Hex.map((data) => (
                 <p>{
                     value === data.id ? data.description : null
